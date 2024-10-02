d32ef7d745c73fa9e33d016d96aeb9acb786a72f
Kriszha Real <yujmin901205@gmail.com>
2024-10-03 04:57:43 +0800

    Add project files.

---
+++ ConsoleAppAssignment/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CONSOLE APP PART 1
+            //string[] animals = { "Cat", "Dog", "Elephant", "Tiger", "Lion" };
+            ////append
+            //Console.WriteLine("Please enter an animal:");
+            //string userInput = Console.ReadLine();
+            //for (int i = 0; i < animals.Length; i++)
+            //{
+            //    animals[i] += " " + userInput;
+            //}
+            //Console.WriteLine("\nUpdated animals:");
+            //foreach (string animal in animals)
+            //{
+            //    Console.WriteLine(animal);
+            //    Console.ReadLine();
+            //}
+
+            //CONSOLE APP PART 2
+            //int c = 0; //counter
+            //while (c < 5)
+            //{
+            //    Console.WriteLine("This loop will end infinite loop and will iterate properly");
+            //    c++;
+            //    Console.ReadLine();
+            //}
+
+            //CONSOLE APP PART 3
+            //int c1 = 0; //counter
+            //while (c1 < 5)
+            //{
+            //    Console.WriteLine("Loop with < operator" + c1);
+            //    c1++;
+            //}
+            //Console.WriteLine();
+
+            //int c2 = 0;
+            //while(c2 <= 5)
+            //{
+            //    Console.WriteLine("Loop with <= operator" + c2);
+            //    c2++;
+            //    Console.ReadLine();
+            //}
+
+            //CONSOLE APP PART 4
+            //List<string> itemsList = new List<string> { "Apple", "Banana", "Cherry", "Date", "Elderberry" };
+
+            //Console.WriteLine("Please enter text to search for in the list:");
+            //string userInput = Console.ReadLine();
+
+            //bool isMatchFound = false;
+
+            //for (int i = 0; i < itemsList.Count; i++)
+            //{
+            //    if (itemsList[i].Equals(userInput, StringComparison.OrdinalIgnoreCase))
+            //    {
+            //        Console.WriteLine($"Match found at index {i}: {itemsList[i]}");
+            //        Console.ReadLine();
+            //        isMatchFound = true;
+            //        break;
+            //    }
+            //}
+
+            //if (!isMatchFound)
+            //{
+            //    Console.WriteLine("The input you provided is not on the list.");
+            //    Console.ReadLine();
+            //}
+
+            //CONSOLE APP PART 5
+            //List<string> itemsList = new List<string> { "Apple", "Banana", "Cherry", "Apple", "Elderberry", "Banana" };
+
+            //Console.WriteLine("Please enter text to search for in the list:");
+            //string userInput = Console.ReadLine();
+
+            //bool isMatchFound = false;
+
+            //for (int i = 0; i < itemsList.Count; i++)
+            //{
+            //    if (itemsList[i].Equals(userInput, StringComparison.OrdinalIgnoreCase))
+            //    {
+            //        Console.WriteLine($"Match found at index {i}: {itemsList[i]}");
+            //        Console.ReadLine();
+            //        isMatchFound = true;
+            //    }
+            //}
+
+            //if (!isMatchFound)
+            //{
+            //    Console.WriteLine("The input you provided is not on the list.");
+            //    Console.ReadLine();
+            //}
+
+            //CONSOLE APP PART 6
+            //List<string> itemsList = new List<string> { "Apple", "Banana", "Cherry", "Apple", "Elderberry", "Banana" };
+            //HashSet<string> seenItems = new HashSet<string>();
+
+            //foreach (string item in itemsList)
+            //{
+            //    if (seenItems.Contains(item))
+            //    {
+            //        Console.WriteLine($"{item} - this item is a duplicate");
+            //        Console.ReadLine();
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{item} - this item is unique");
+            //        seenItems.Add(item);
+            //        Console.ReadLine();
+            //    }
+            //}
+        }
+    }
+}
